b17c1f7482abd54df11cec33562fa236c0435ec3
Vitor Vianna <vitormatheusfv@gmail.com>
2023-09-30 15:31:22 -0300

    Maps

--- 10-sets-maps/script.js
+++ 10-sets-maps/script.js
@@ -87,3 +87,65 @@ const colaboradores = [
 
 const cargos = new Set(colaboradores).size;
 console.log(cargos);
+
+// MAPS - Utiliza keys e values, porém as keys podem ser valores além de strings
+const restMap = new Map();
+// seta uma nova key e value
+restMap.set("name", "Classico Italiano");
+restMap.set(1, "Firenze, Italy");
+restMap.set(2, "Lisbon, Portugal");
+console.log(restMap);
+// set linkado
+restMap
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "Estamos abertos :D")
+  .set(false, "Estamos fechados :(");
+// método get passando key
+console.log(restMap.get("name"));
+// utilizando as keys boolean
+const horario = 21;
+console.log(
+  restMap.get(horario > restMap.get("open") && horario < restMap.get("close"))
+);
+
+console.log(restMap.has("categories"));
+restMap.delete(2);
+console.log(restMap.size);
+restMap.clear();
+console.log(restMap);
+
+const arr = [1, 2];
+restMap.set(arr, "Test");
+console.log(restMap);
+console.log(restMap.get(arr));
+
+restMap.set(document.querySelector("h1"), "Heading");
+console.log(restMap);
+// Convertendo objetos para maps
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+// Outro método de adição
+const pergunta = new Map([
+  ["pergunta", "Qual é a melhor linguagem de programação?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "Javascript"],
+  ["correct", 3],
+  [true, "Correto!"],
+  [false, "Tente de novo"],
+]);
+console.log(pergunta.get("pergunta"));
+for (const [key, value] of pergunta.entries()) {
+  if (typeof key === "number") console.log(`${key}: ${value}`);
+}
+// const resposta = Number(prompt("Sua resposta: "));
+const resposta = 3;
+console.log(pergunta.get(pergunta.get("correct") === resposta));
+
+// Convertendo map para array
+console.log([...pergunta]);
+console.log([...pergunta.keys()]);
+console.log([...pergunta.values()]);
