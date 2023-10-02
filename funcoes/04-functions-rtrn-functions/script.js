c1d874df72143582013d24c167f80b84ad285cf5
Vitor Vianna <vitormatheusfv@gmail.com>
2023-10-02 18:21:20 -0300

    Funcoes retornando funcoes

---
+++ funcoes/04-functions-rtrn-functions/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const greet = (greeting) => (name) => console.log(`${greeting} ${name}`);
+const greeterOla = greet("Olá!");
+
+greeterOla("Vitor");
+greeterOla("Marcos");
+
+greet("Oi")("Ana");
