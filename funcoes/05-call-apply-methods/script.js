c9318c870f3d371baabaacea06173c1d436ab004
Vitor Vianna <vitormatheusfv@gmail.com>
2023-10-03 17:17:10 -0300

    Bind methods

--- funcoes/05-call-apply-methods/script.js
+++ funcoes/05-call-apply-methods/script.js
@@ -45,6 +45,47 @@ console.log(swiss);
 
 // Método apply (ANTIGO)
 const flightData = [583, "George Cooper"];
-book.apply(swiss, flightData);
-// Método atual
+// book.apply(swiss, flightData);
+// Método atual - CALL
 book.call(swiss, ...flightData);
+
+// Método bind - Seta o this keyword e a função para uma nova variável
+const bookEW = book.bind(eurowings);
+bookEW(23, "Steve Williams");
+const bookLX = book.bind(swiss);
+const bookLH = book.bind(lufthansa);
+// Setar valores padrões para os argumentos com bind
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23("Ana Lucia");
+
+// Com Event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(`New plane buyed for ${this.airline}`);
+
+  this.planes++;
+  console.log(`Now ${this.airline} has ${this.planes} planes`);
+};
+
+document
+  .querySelector(".buy")
+  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+// Aplicação parcial - parâmetros pré definidos
+// Função geral
+const addTax = function (rate, value) {
+  return value + value * rate;
+};
+
+// Aplicação parcial
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(150));
+
+// Refatorando com funções retornando funções
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(150));
