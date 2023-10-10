b384eb43c683cfb8903c28af202c7f9d02a7a4c8
Vitor Vianna <vitormatheusfv@gmail.com>
2023-10-10 18:37:54 -0300

    Desafio 03

--- arrays/04-code-challenge-01/script.js
+++ arrays/04-code-challenge-01/script.js
@@ -58,3 +58,93 @@ const calcIdadeHumana = function (idadesCaes) {
 
 console.log(calcIdadeHumana(idadesCaes1));
 console.log(calcIdadeHumana(idadesCaes2));
+
+// Desafio 03
+/* Uma alimentação OK para os cães é tolerável apenas 10% abaixo e 10% acima da porção recomendada 
+
+1. Percorra o array dogs e para cada cachorro, calcule a porção recomendada de comida e adicione ao objeto como uma nova propriedade. Não crie um novo array
+Formula: comidaRecomendada = peso ** 0.75 * 28. (O resultado é em gramas de comida, e o peso precisa ser em kg)
+
+2. Encontre o cão de Sarah e log no console se ele está comendo demais ou de menos. DICA: alguns cães tem multiplos donos, então primeiro você precisa achar Sarah no array de donos
+
+3. Crie um array contendo todos donos de cães que comem demais ('donosComemMais') e um array com os donos de cães que comem de menos ('donosComemMenos')
+
+4. Log uma string no console para cada array criado no 3. Ex: "os cães de Mathilda e Alice e Bob comem demais!"
+
+5. Log no console se existe um cachorro comendo exatamente a quantidade de comida recomendada (apenas true ou false)
+
+6. Log no console se existe algum cachorro comendo uma quantidade OK de comida (apenas true ou false)
+
+7. Crie um array contendo os cães que comem uma quantidade OK de comida (tente reusar a condição do 6)
+
+8. Crie uma cópia superficial do array de cães e ordene pela quantia recomendada de comida em ordem crescente (tenha em mente as porções dentro do objeto de arrays)
+*/
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+const recomendado = function (dog) {
+  if (dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1) {
+    return 'no limite';
+  } else if (dog.curFood > dog.recFood * 1.1) {
+    return 'acima';
+  } else {
+    return 'abaixo';
+  }
+};
+// 1.
+dogs.map(dog => (dog.recFood = Number((dog.weight ** 0.75 * 28).toFixed(2))));
+console.log(dogs);
+// 2.
+console.log(
+  dogs
+    .filter(dog => dog.owners.includes('Sarah'))
+    .reduce((resultado, dog) => {
+      resultado = `O cão de Sarah está comendo ${recomendado(
+        dog
+      )} do recomendado`;
+      return resultado;
+    }, '')
+);
+// 3.
+const {
+  comemMais: [comemMais],
+  comemMenos: [comemMenos],
+} = dogs
+  .filter(dog => !(recomendado(dog) === 'no limite'))
+  .reduce(
+    (arrays, dog) => {
+      arrays[recomendado(dog) === 'acima' ? 'comemMais' : 'comemMenos'].push(
+        dog.owners
+      );
+      return arrays;
+    },
+    { comemMais: [], comemMenos: [] }
+  );
+console.log(comemMais, comemMenos);
+// 4.
+console.log(`Os cães de ${comemMais.join(' e ')} comem demais.`);
+console.log(`Os cães de ${comemMenos.join(' e ')} comem de menos.`);
+// 5.
+console.log(dogs.some(dog => dog.curFood === dog.recFood));
+// 6.
+const comeRecomendado = dog => recomendado(dog) === 'no limite';
+console.log(dogs.some(comeRecomendado));
+// 7.
+const [arrayIdeal] = dogs.filter(comeRecomendado).reduce((array, dog) => {
+  array.push(dog);
+  return array;
+}, []);
+console.log(arrayIdeal);
+// 8.
+const recOrdenada = dogs
+  .reduce((novoArray, dog) => {
+    novoArray.push(dog.recFood);
+    return novoArray;
+  }, [])
+  .sort((a, b) => a - b);
+console.log(recOrdenada);
--- arrays/07-criar-preencher-arrays/script.js
+++ arrays/07-criar-preencher-arrays/script.js
@@ -112,3 +112,51 @@ labelBalance.addEventListener('click', function () {
   );
   console.log(movementsUI);
 });
+
+////////////////////////////////////////////////////////////////////////////////
+// 1
+const somaDepositoBancario = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(somaDepositoBancario);
+// 2
+const somaDeposito1000 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+console.log(somaDeposito1000);
+// 3
+const { depositos, saques } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (somas, transacao) => {
+      // transacao > 0
+      //   ? (somas.depositos += transacao)
+      //   : (somas.saques += transacao);
+      somas[transacao > 0 ? 'depositos' : 'saques'] += transacao;
+      return somas;
+    },
+    { depositos: 0, saques: 0 }
+  );
+console.log(depositos, saques);
+// 4
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const excecoes = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(palavra =>
+      excecoes.includes(palavra) ? palavra : capitalize(palavra)
+    )
+    .join(' ');
+
+  return capitalize(titleCase);
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONGE title'));
+console.log(convertTitleCase('but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
